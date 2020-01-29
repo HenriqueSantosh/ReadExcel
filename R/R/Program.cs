97d728de94a4772bb77ee3a6d3f813138d5610cb
henriqueLimaSanto <henriquegualberto33@gmail.com>
2020-01-29 08:39:52 -0300

    REad Excel With C#

---
+++ R/R/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+       
+        public static void readExcel()
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+            }
+        }
+}
